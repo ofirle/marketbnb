ab7af0aa6d9cc1542ccd2fab3ee0b341d5aaa7f5
Ofir Levy <ofirle92@gmail.com>
2023-07-18 00:36:49 +0300

    useAxios Hook

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,49 +1,47 @@
 import {useState, useEffect} from 'react';
-import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
 
 axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 
-interface UseAxiosResult<T> {
-    response?: AxiosResponse<T>;
-    error?: AxiosError;
-    loading: boolean;
-    sendData: () => void;
-}
-
-const useAxios = (configObj) => {
-
-    const {
-        axiosInstance,
-        method, url, requestConfig
-    } = configObj;
-    const [response, setResponse] = useState([]);
+type methodTypes = 'get' | 'post' | 'delete' | 'put' | 'patch';
+const useAxios = () => {
+    const [response, setResponse] = useState<any>([]);
     const [error, setError] = useState('');
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [controller, setController] = useState<AbortController>();
+
+    const axiosFetch = async ({axiosInstance, method, url, requestConfig = {}}: {
+        axiosInstance: AxiosInstance,
+        method: methodTypes,
+        url: string,
+        requestConfig?: AxiosRequestConfig
+    }) => {
+        try {
+            setLoading(true);
+            const ctrl = new AbortController();
+            setController(ctrl);
+            const res = await axiosInstance[method](url, {
+                ...requestConfig,
+                signal: ctrl.signal
+            });
+            setResponse(res.data);
+        } catch (err: any) {
+            console.log(err.message);
+            setError(err.message)
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        const controller = new AbortController();
-        const fetchData = async () => {
-            try{
-                const res = await axiosInstance[method](url, {
-                    ...requestConfig,
-                    signal: controller.signal
-                });
-                setResponse(res.data);
-            } catch (err) {
-                console.log(err.message);
-                setError(err.message)
+        return () => {
+            if (controller) {
+                controller.abort()
             }
-            finally {
+        };
+    }, [controller]);
 
-            }
-        }
-
-        fetchData();
-
-        return () => controller.abort();
-    }, []);
-
-    return {response, error, loading};
+    return {response, error, loading, axiosFetch};
 };
 
 export default useAxios;
